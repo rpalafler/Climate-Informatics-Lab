32d03fcde9d14dd6fe2116adb8f21533dfc31be9
malbo-1 <malbo1974@sdsu.edu>
2024-01-12 12:39:00 -0800

    reset directory

---
+++ Michael_Albo/DeckPractice/bitmap_example/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import BitMapComponent from './BitMapComponent';
+import './App.css';
+
+const App =()=> {
+  return (
+    <div className="App">
+     <h1>BitMap Example</h1>
+     <BitMapComponent/>
+    </div>
+  );
+};
+
+export default App;
